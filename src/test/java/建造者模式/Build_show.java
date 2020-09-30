5c7c0fccaf8e75e7aadcebbcf3df81735a6397ab
xzs-xx <2993424163@qq.com>
2020-09-30 09:53:29 +0800

    CI-CD

---
+++ src/test/java/建造者模式/Build_show.java
@@ -0,0 +1,22 @@
+package 建造者模式;
+
+import org.junit.jupiter.api.Test;
+
+import build_case.Meal;
+import build_case.MealBuilder;
+
+public class Build_show {
+	@Test
+	public void showbuild() {
+		MealBuilder mealBuilder = new MealBuilder();
+		Meal vegMeal = mealBuilder.prepareVegMeal();
+		System.out.println("Veg Meal");
+		vegMeal.showItems();
+		System.out.println("Total Cost: " +vegMeal.getCost());
+	 
+		Meal nonVegMeal = mealBuilder.prepareNonVegMeal();
+		System.out.println("\n\nNon-Veg Meal");
+		nonVegMeal.showItems();
+		System.out.println("Total Cost: " +nonVegMeal.getCost());
+	}
+}
